48494bf3453f0ff0ee5a79836c8c5aacd8110cb9
cythonista <gnfx0514@gmail.com>
2020-06-23 21:45:55 +0900

    add configuration

---
+++ src/main/java/com/memo/ui/configuration/ApiCallConfigurationProperties.java
@@ -0,0 +1,26 @@
+package com.memo.ui.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "workshop.api.call")
+public class ApiCallConfigurationProperties {
+
+    private final String host;
+    private final int port;
+
+    public ApiCallConfigurationProperties(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getHost() {
+        return this.host;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+
+}
