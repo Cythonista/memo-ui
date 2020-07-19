b719bf4b70ae3fbc40c286fda9c84e89d1083e1e
cythonista <gnfx0514@gmail.com>
2020-07-19 23:05:31 +0900

    add test method

--- src/test/java/com/memo/ui/controller/CardControllerTest.java
+++ src/test/java/com/memo/ui/controller/CardControllerTest.java
@@ -1,7 +1,6 @@
 package com.memo.ui.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import com.memo.ui.UiApplication;
@@ -18,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
